7dc3f552b3a6cfcddc6681f2bf449fbdda0590d7
Stian Reistad Røgeberg <srr@dips.no>
2023-12-11 15:12:31 +0100

    Testing nav and side bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,120 @@
 
 import './App.css';
 
+const SearcBar = () => {
+  return (
+    <div>
+      <ul>
+        <li><a class="active" href="#home">Home</a></li>
+        <li><a href="#news">News</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#about">About</a></li>
+      </ul>
+    </div>
+  )
+};
+
+const SideNav = (props) => {
+  return (
+    <div>
+      <ul>
+        <li><a class="active" href="#home">Home</a></li>
+        <li><a href="#news">News</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#about">About</a></li>
+      </ul>
+      </div>
+  );
+};
+
 function App() {
   return (
-    <div>Hello world</div>
+    <div>
+       <header>
+    <nav class="fixed-navbar">
+      <div class="navbar">
+        <div class="logo">Your Logo</div>
+        <div class="search">
+          <input type="text" placeholder="Search..." />
+        </div>
+        <ul class="nav-links">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+
+  <aside class="sidebar">
+    <ul class="sidebar-links">
+      <li><a href="#">Filtrer på foretak</a></li>
+      <li><a href="#">Profile</a></li>
+      <li><a href="#">Settings</a></li>
+      <li><a href="#">Logout</a></li>
+    </ul>
+  </aside>
+
+  <div class="content">
+    <h1>Main Content Area</h1>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+    <p>lasdkjldjkljasd</p>
+  </div>
+    </div>
   );
 }
 
