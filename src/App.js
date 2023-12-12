b8346c824079c9af7354444bc99c40d96ba27f1c
Stian Reistad Røgeberg <srr@dips.no>
2023-12-12 15:56:07 +0100

    Added components for layout

--- src/App.js
+++ src/App.js
@@ -3,42 +3,15 @@ import './App.css';
 
 const SearcBar = () => {
   return (
-    <div>
-      <ul>
-        <li><a class="active" href="#home">Home</a></li>
-        <li><a href="#news">News</a></li>
-        <li><a href="#contact">Contact</a></li>
-        <li><a href="#about">About</a></li>
-      </ul>
-    </div>
-  )
-};
-
-const SideNav = (props) => {
-  return (
-    <div>
-      <ul>
-        <li><a class="active" href="#home">Home</a></li>
-        <li><a href="#news">News</a></li>
-        <li><a href="#contact">Contact</a></li>
-        <li><a href="#about">About</a></li>
-      </ul>
-      </div>
-  );
-};
-
-function App() {
-  return (
-    <div>
-       <header>
+    <header>
     <nav class="fixed-navbar">
       <div class="navbar">
         <div class="logo">Your Logo</div>
         <div class="search">
-          <input type="text" placeholder="Search..." />
+          <input type="text" placeholder="Søk i saker..." />
         </div>
         <ul class="nav-links">
-          <li><a href="#">Home</a></li>
+          <li><a href="#">Logg inn</a></li>
           <li><a href="#">About</a></li>
           <li><a href="#">Services</a></li>
           <li><a href="#">Contact</a></li>
@@ -46,17 +19,12 @@ function App() {
       </div>
     </nav>
   </header>
+  )
+};
 
-  <aside class="sidebar">
-    <ul class="sidebar-links">
-      <li><a href="#">Filtrer på foretak</a></li>
-      <li><a href="#">Profile</a></li>
-      <li><a href="#">Settings</a></li>
-      <li><a href="#">Logout</a></li>
-    </ul>
-  </aside>
-
-  <div class="content">
+const MainContent = () => {
+  return (
+    <div class="content">
     <h1>Main Content Area</h1>
     <p>lasdkjldjkljasd</p>
     <p>lasdkjldjkljasd</p>
@@ -114,6 +82,119 @@ function App() {
     <p>lasdkjldjkljasd</p>
     <p>lasdkjldjkljasd</p>
   </div>
+  );
+};
+
+const SideNav = (props) => {
+  return (
+    <aside class="sidebar">
+    <ul class="sidebar-links">
+      <li><a href="#">Filtrer på foretak</a></li>
+      <li><a href="#">Fra dato</a></li>
+      <li><a href="#">Til dato</a></li>
+      <li><a href="#">Produkt</a></li>
+    </ul>
+  </aside>
+  );
+};
+
+// function App2() {
+//   return (
+//     <div>
+//        <header>
+//     <nav class="fixed-navbar">
+//       <div class="navbar">
+//         <div class="logo">Your Logo</div>
+//         <div class="search">
+//           <input type="text" placeholder="Søk i saker..." />
+//         </div>
+//         <ul class="nav-links">
+//           <li><a href="#">Logg inn</a></li>
+//           <li><a href="#">About</a></li>
+//           <li><a href="#">Services</a></li>
+//           <li><a href="#">Contact</a></li>
+//         </ul>
+//       </div>
+//     </nav>
+//   </header>
+
+//   <aside class="sidebar">
+//     <ul class="sidebar-links">
+//       <li><a href="#">Filtrer på foretak</a></li>
+//       <li><a href="#">Fra dato</a></li>
+//       <li><a href="#">Til dato</a></li>
+//       <li><a href="#">Produkt</a></li>
+//     </ul>
+//   </aside>
+
+//   <div class="content">
+//     <h1>Main Content Area</h1>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//     <p>lasdkjldjkljasd</p>
+//   </div>
+//     </div>
+//   );
+// }
+
+function App() {
+  return (
+    <div>
+    <SearcBar />
+    <SideNav />
+    <MainContent />
     </div>
   );
 }
